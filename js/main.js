b4944c93a7cf78661477da30cd003e4642365686
Elaine Calasans <calasanssouza123@gmail.com>
2020-02-10 15:53:26 -0300

    evrsao 1

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+
+var d = new Date();
+alert(d.getMonth());
+lert(d.getHours());
+
+
+
+/*
+LAÇO REPETIÇÃO
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+};
+*/
+
+/* ESTRUTURA DE REPETIÇÃO
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+};
+*/
+
+
+/*var idade = prompt("Qual sua idade");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+
+/*
+DICIONARIO
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+/*
+var lista = ["maça", "pêra", "limão"];
+lista.push("uva");
+//lista.pop();
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+
+
+
+/*var nome = "Elaine Calasans";
+//var n1 = 30;
+//var n2 = 10;
+//var frase = "Karatê é um excelente esporte";
+//alert("Bem vindo " + " tem " + idade + " anos ");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+alert(frase.replace("Brasil", "Eu"));
+*/
